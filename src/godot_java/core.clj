3c3de5933f2e48710f09e49ba44592544b4573f9
3starblaze <3starblaze@protonmail.com>
2024-08-12 18:55:42 +0300

    "Implement" struct-like type generation
    Also added a cleaner way to inject package name.

--- src/godot_java/core.clj
+++ src/godot_java/core.clj
@@ -19,8 +19,6 @@
 (defn indent-line [s]
   (str "    " s))
 
-(defn package-string [package-name]
-  (format "package %s;" package-name))
 
 (def dont-use-me-class-name "GDDontUseMe")
 
@@ -28,9 +26,7 @@
   (if (= s "new") "gd_new" s))
 
 (def dont-use-me-class-lines
-  [(package-string base-package-name)
-   ""
-   "// This is a dummy class for types that could not be converted (C pointers)"
+  ["// This is a dummy class for types that could not be converted (C pointers)"
    (format "class %s extends Object {}" dont-use-me-class-name)])
 
 (defn resolve-arg-type [?s]
@@ -48,6 +44,12 @@
     :else (->> (str/split ?s #"::" 2)
                last
                (str godot-class-prefix))))
+
+(defn resolve-member-type [s]
+  (case s
+    "float" "float"
+    "int32" "int"
+    (str godot-class-prefix s)))
 
 (defn convert-parameter-name [s]
   (str godot-parameter-prefix s))
@@ -102,37 +104,46 @@
 (defn normal-constant-m->line [m]
   (format "public final static long %s = %s;" (get m "name") (get m "value")))
 
-(defn normal-class-m->lines [m]
-  (concat
-   [(package-string base-package-name)
-    ""]
-   (class-lines (str godot-class-prefix (get m "name"))
-                (if-let [inherits (get m "inherits")]
-                  (resolve-arg-type inherits)
-                  "Object")
-                (-> (concat
-                     (map normal-constant-m->line (get m "constants"))
-                     (map enum-m->lines (get m "enums"))
-                     (map method-m->lines (get m "methods")))
-                    flatten))))
+(defn normal-class-m->build-file-export-m [m]
+  (let [classname (str godot-class-prefix (get m "name"))]
+    {:filename (str classname ".java")
+     :lines (class-lines classname
+                         (if-let [inherits (get m "inherits")]
+                           (resolve-arg-type inherits)
+                           "Object")
+                         (-> (concat
+                              (map normal-constant-m->line (get m "constants"))
+                              (map enum-m->lines (get m "enums"))
+                              (map method-m->lines (get m "methods")))
+                             flatten))}))
+
+(defn struct-like-class-m->build-file-export-m [m]
+  (let [classname (resolve-arg-type (get m "name"))]
+    {:filename (str classname ".java")
+     ;; TODO: Implement body
+     :lines (class-lines classname "Object" [])}))
+
+(defn make-struct-like-classes []
+  (let [classes (as-> api it
+                  (get it "builtin_class_member_offsets")
+                  (filter #(= (get % "build_configuration") "float_64") it)
+                  (first it)
+                  (get it "classes"))]
+    (map struct-like-class-m->build-file-export-m classes)))
+
 
 (defn generate-and-save-java-classes []
   (.mkdirs java-class-build-dir)
-  (spit (io/file java-class-build-dir (str dont-use-me-class-name ".java"))
-        (str/join "\n" dont-use-me-class-lines))
-  (doseq [[classname source] (map #(vector (get % "name")
-                                           (str/join "\n" (normal-class-m->lines %)))
-                                  (get api "classes"))]
-    (spit (io/file java-class-build-dir (str (resolve-arg-type classname) ".java"))
-          source)))
-
-(comment
-  (->> (get api "classes")
-       (filter #(= (get % "name") "Object"))
-       first
-       normal-class-m->lines
-       flatten
-       (str/join "\n")
-       println))
+  (doseq [{:keys [filename lines]}
+          (concat
+           [{:filename (str dont-use-me-class-name ".java")
+             :lines (class-lines dont-use-me-class-name "Object" [])}]
+           (map normal-class-m->build-file-export-m (get api "classes"))
+           (make-struct-like-classes))]
+    (spit (io/file java-class-build-dir filename)
+          (str/join "\n" (concat
+                          [(format "package %s;" base-package-name)
+                           ""]
+                          lines)))))
 
 (comment (generate-and-save-java-classes))
